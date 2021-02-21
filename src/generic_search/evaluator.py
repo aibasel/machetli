f201e8bd05e2251a1576d87ebe7f2cafadd9f211
Lucas <lucas.galerykaeser@gmail.com>
2021-02-21 21:35:53 +0100

    [generic_search_skeleton] Make draft of main search function and evaluation and succ. gen. class.

--- src/generic_search/evaluator.py
+++ src/generic_search/evaluator.py
@@ -1,3 +1,12 @@
+
+
+import subprocess
+
 class Evaluator():
     def evaluate(self, state):
         pass
+
+    def run_tasks(self, state):
+        completed_process = subprocess.run(self.call_string_manager.call_string,
+                                           text=True, capture_output=True, shell=True)
+        return results
--- src/generic_search/search.py
+++ src/generic_search/search.py
@@ -1,5 +1,3 @@
 def first_choice_hill_climbing(initial_state, successor_generators, evaluator):
     if not isinstance(successor_generators, list):
         successor_generators = [successor_generators]
-    
-        
