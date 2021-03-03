b4bc50c8763b06d0d6fbbce90c4cc4facf69fbf5
Lucas <lucas.galerykaeser@gmail.com>
2021-03-03 18:01:47 +0100

    [generic_search_skeleton] Start transferring legacy code for successor generation and hill-climbing search.

--- src/generic_search/search.py
+++ src/generic_search/search.py
@@ -1,19 +1,22 @@
 
+import copy
+import subprocess
+from downward_lib import pddl_parser, timers
+import sys
 import os
 dirname = os.path.dirname(__file__)
 downward_lib = os.path.join(dirname, "downward_lib/")
-import sys
 sys.path.append(downward_lib)
-from downward_lib import pddl_parser, timers
-import subprocess
-import copy
+from pddl_writer import write_PDDL
 
 
 NEW_DOMAIN_FILENAME = "minimized_domain.pddl"
 NEW_PROBLEM_FILENAME = "minimized_problem.pddl"
 
+
 def read_task(dom_filename, prob_filename):
     return pddl_parser.open(domain_filename=dom_filename, task_filename=prob_filename)
+
 
 def run_tasks(state, parsers):
     if not isinstance(parsers, list):
@@ -44,9 +47,26 @@ def first_choice_hill_climbing(initial_state, successor_generators, evaluator):
     if "pddl_task" in initial_state:
         is_pddl_task = True
     task = initial_state["pddl_task"] if is_pddl_task else initial_state["sas_task"]
+    current_state = initial_state
+    eve = evaluator()
 
-    with timers.timing("Searching for smaller task using first-choice hill-climbing"):
+    with timers.timing("Starting first-choice hill-climbing search"):
         for succ_gen in successor_generators:
-            pass
+            with timers.timing("Generating successors with class {}".format(succ_gen.__name__)):
+                current_task = current_state["pddl_task"] if is_pddl_task else current_state["sas_task"]
+                children = 0
+                num_successors = 0
+                print()
+                while True:
+                    if children > 0:
+                        print("child found ({}), searched through {} successor(s)\n".format(children, num_successors)))
+                    num_successors = 0
+                    children += 1
+                    for successor_task, removed_element in succ_gen.get_successors(current_task):
+                        num_successors += 1
+                        write_PDDL(successor_task, NEW_DOMAIN_FILENAME, NEW_PROBLEM_FILENAME)
+                        current_state = update_state(current_state, successor_task)
+                        if eve.evaluate(current_state):
+                            
 
 
--- src/generic_search/successor_generator.py
+++ src/generic_search/successor_generator.py
@@ -1,3 +1,20 @@
+
+from visitor import TaskElementEraseActionVisitor
+import copy
+from downward_lib import timers
+import random
+
 class SuccessorGenerator():
     def get_successors(self, state):
         pass
+
+class RemoveActions(SuccessorGenerator):
+    def get_successors(self, state):
+        task = state["pddl_task"]
+        action_names = [action.name for action in task.actions]
+        random.Random().shuffle(action_names)
+        for name in action_names:
+            pre_child = copy.deepcopy(task)
+            with timers.timing("Obtaining successor"):
+                child = pre_child.accept(TaskElementEraseActionVisitor(name))
+            yield child
