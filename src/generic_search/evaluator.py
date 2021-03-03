ba1758d5b0f3a0fc2315a0746e7e9790d905bdd0
Lucas <lucas.galerykaeser@gmail.com>
2021-03-03 16:53:22 +0100

    [generic_search_skeleton] Move run_tasks function to search module; add downward_lib to system path; start transferring search code from legacy code.

--- src/generic_search/evaluator.py
+++ src/generic_search/evaluator.py
@@ -1,29 +1,4 @@
 
-from lab.experiment import Experiment
-import subprocess
-from lab.tools import Properties
-import copy
-
-
 class Evaluator():
     def evaluate(self, state):
         pass
-
-    def run_tasks(self, state, parsers):
-        if not isinstance(parsers, list):
-            parsers = [parsers]
-        results = {}
-        props = Properties()
-        for call in state["call_strings"]:
-            output = subprocess.run(state["call_strings"][call], text=True,
-                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
-            with open("run.log", "w") as f:
-                f.write(output)
-
-            results[call] = {}
-            for parser in parsers:
-                parser.parse()
-                results[call].update(copy.deepcopy(parser.props))
-
-        subprocess.run(["rm", "properties", "run.log", "sas_plan"])
-        return results
--- src/generic_search/search.py
+++ src/generic_search/search.py
@@ -1,3 +1,52 @@
+
+import os
+dirname = os.path.dirname(__file__)
+downward_lib = os.path.join(dirname, "downward_lib/")
+import sys
+sys.path.append(downward_lib)
+from downward_lib import pddl_parser, timers
+import subprocess
+import copy
+
+
+NEW_DOMAIN_FILENAME = "minimized_domain.pddl"
+NEW_PROBLEM_FILENAME = "minimized_problem.pddl"
+
+def read_task(dom_filename, prob_filename):
+    return pddl_parser.open(domain_filename=dom_filename, task_filename=prob_filename)
+
+def run_tasks(state, parsers):
+    if not isinstance(parsers, list):
+        parsers = [parsers]
+    results = {}
+    props = Properties()
+    for call in state["call_strings"]:
+        output = subprocess.run(state["call_strings"][call], text=True,
+                                stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout
+        with open("run.log", "w") as f:
+            f.write(output)
+
+        results[call] = {}
+        for parser in parsers:
+            parser.parse()
+            results[call].update(copy.deepcopy(parser.props))
+
+    for file in ["properties", "run.log", "sas_plan"]:
+        os.remove(file)
+
+    return results
+
+
 def first_choice_hill_climbing(initial_state, successor_generators, evaluator):
     if not isinstance(successor_generators, list):
         successor_generators = [successor_generators]
+    is_pddl_task = False
+    if "pddl_task" in initial_state:
+        is_pddl_task = True
+    task = initial_state["pddl_task"] if is_pddl_task else initial_state["sas_task"]
+
+    with timers.timing("Searching for smaller task using first-choice hill-climbing"):
+        for succ_gen in successor_generators:
+            pass
+
+
