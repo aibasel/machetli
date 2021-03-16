a645978eb137bf536dca7e9b21be1078baad20bd
Lucas <lucas.galerykaeser@gmail.com>
2021-03-16 15:00:39 +0100

    [generic_search_skeleton] Tansfer transformer classes to successor_generator module and adapt them and rename visitor module to visitors.

--- minimizer/successor_generator.py
+++ minimizer/successor_generator.py
@@ -1,12 +1,14 @@
 
-from minimizer.visitor import TaskElementEraseActionVisitor
+from minimizer import visitors
 import copy
 from minimizer.downward_lib import timers
 import random
 
+
 class SuccessorGenerator():
     def get_successors(self, state):
         pass
+
 
 class RemoveActions(SuccessorGenerator):
     def get_successors(self, state):
@@ -17,6 +19,70 @@ class RemoveActions(SuccessorGenerator):
             child_state = copy.deepcopy(state)
             pre_child_task = child_state["pddl_task"]
             with timers.timing("Obtaining successor"):
-                child_task = pre_child_task.accept(TaskElementEraseActionVisitor(name))
+                child_task = pre_child_task.accept(
+                    visitors.TaskElementEraseActionVisitor(name))
+            child_state["pddl_task"] = child_task
+            yield child_state
+
+
+class ReplaceAtomsWithTruth(SuccessorGenerator):
+    def get_successors(self, state):
+        task = state["pddl_task"]
+        predicate_names = [predicate.name for predicate in task.predicates if
+                           not (predicate.name == "dummy_axiom_trigger" or predicate.name == "=")]
+        random.Random().shuffle(predicate_names)
+        for name in predicate_names:
+            child_state = copy.deepcopy(state)
+            pre_child_task = child_state["pddl_task"]
+            with timers.timing("Obtaining successor"):
+                child_task = pre_child_task.accept(
+                    visitors.TaskElementErasePredicateTrueAtomVisitor(name))
+            child_state["pddl_task"] = child_task
+            yield child_state
+
+
+class ReplaceAtomsWithFalsity(SuccessorGenerator):
+    def get_successors(self, state):
+        task = state["pddl_task"]
+        predicate_names = [predicate.name for predicate in task.predicates if
+                           not (predicate.name == "dummy_axiom_trigger" or predicate.name == "=")]
+        random.Random().shuffle(predicate_names)
+        for name in predicate_names:
+            child_state = copy.deepcopy(state)
+            pre_child_task = child_state["pddl_task"]
+            with timers.timing("Obtaining successor"):
+                child_task = pre_child_task.accept(
+                    visitors.TaskElementErasePredicateFalseAtomVisitor(name))
+            child_state["pddl_task"] = child_task
+            yield child_state
+
+
+class ReplaceLiteralsWithTruth(SuccessorGenerator):
+    def get_successors(self, state):
+        task = state["pddl_task"]
+        predicate_names = [predicate.name for predicate in task.predicates if
+                           not (predicate.name == "dummy_axiom_trigger" or predicate.name == "=")]
+        random.Random().shuffle(predicate_names)
+        for name in predicate_names:
+            child_state = copy.deepcopy(state)
+            pre_child_task = child_state["pddl_task"]
+            with timers.timing("Obtaining successor"):
+                child_task = pre_child_task.accept(
+                    visitors.TaskElementErasePredicateTrueLiteralVisitor(name))
+            child_state["pddl_task"] = child_task
+            yield child_state
+
+
+class RemoveObjects(SuccessorGenerator):
+    def get_successors(self, state):
+        task = state["pddl_task"]
+        object_names = [obj.name for obj in task.objects]
+        random.Random().shuffle(object_names)
+        for name in object_names:
+            child_state = copy.deepcopy(state)
+            pre_child_task = child_state["pddl_task"]
+            with timers.timing("Obtaining successor"):
+                child_task = pre_child_task.accept(
+                    visitors.TaskElementEraseObjectVisitor(name))
             child_state["pddl_task"] = child_task
             yield child_state
