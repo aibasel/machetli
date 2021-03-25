1deeb00d418e826894800c3958088b3574b9edcd
Lucas <lucas.galerykaeser@gmail.com>
2021-03-25 10:49:29 +0100

    [generic_search_skeleton] Delete legacy code.

--- legacy_code/parserTestImpl.py
+++
@@ -1,8 +0,0 @@
-from parserbase import ParserBase
-
-
-class Parser(ParserBase):
-    def parse_output_string(self, output_string) -> bool:
-        cond1 = "caught signal 11 -- exiting" in output_string
-        cond2 = "caught signal 6 -- exiting" in output_string
-        return cond1 or cond2
