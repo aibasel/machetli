c6b3ce132494dc3f6439af08175a0b0e0de02ca3
Lucas <lucas.galerykaeser@gmail.com>
2021-04-06 12:53:25 +0200

    [run_class] Implement functions for the handling of temporary files.

--- minimizer/aux.py
+++ minimizer/aux.py
@@ -1,11 +1,10 @@
+import tempfile
+import contextlib
+import os
+
 from minimizer.sas_reader import sas_file_to_SASTask
+from minimizer.pddl_writer import write_PDDL
 from minimizer.downward_lib import pddl_parser
-
-NEW_DOMAIN_FILENAME = "minimized-domain.pddl"
-NEW_PROBLEM_FILENAME = "minimized-problem.pddl"
-NEW_SAS_FILENAME = "minimized.sas"
-
-# TODO: Handle temp files
 
 
 def parse_pddl_task(dom_filename, prob_filename):
@@ -50,9 +49,79 @@ def run_and_parse_all(state, parsers):
     parsers = [parsers] if not isinstance(parsers, list) else parsers
     for name, result in results.items():
         parsed_results.update(
-            {name: {"stdout": {}, "stderr": {}, "returncode": result["returncode"]}}
+            {name: {"stdout": {}, "stderr": {},
+                    "returncode": result["returncode"]}}
         )
         for parser in parsers:
-            parsed_results[name]["stdout"].update(parser.parse(name, result["stdout"]))
-            parsed_results[name]["stderr"].update(parser.parse(name, result["stderr"]))
+            parsed_results[name]["stdout"].update(
+                parser.parse(name, result["stdout"]))
+            parsed_results[name]["stderr"].update(
+                parser.parse(name, result["stderr"]))
     return parsed_results
+
+
+def generate_sas_file(state, sas_task_key="sas_task"):
+    """
+    Generates a temporary file containing the dump of the SAS+ task from 
+    *state[sas_task_key]* and returns its full path. When the temporary 
+    file is not needed anymore, it can be removed with *os.remove(path)* 
+    (it is not removed automatically).
+    """
+    f = tempfile.NamedTemporaryFile(mode="w+t", suffix=".sas", delete=False)
+    state[sas_task_key].output(f)
+    f.close()
+    return f.name
+
+
+def generate_pddl_files(state, pddl_task_key="pddl_task"):
+    """
+    Generates temporary files containing the dumps of the domain and problem
+    description of the PDDL task from *state[pddl_task_key]* and returns a
+    tuple of the two full paths. When the temporary files are not needed
+    anymore, they can both be removed with *os.remove(path)* (they are not
+    removed automatically).
+    """
+    domain_f = tempfile.NamedTemporaryFile(
+        mode="w+t", suffix=".pddl", delete=False)
+    domain_f.close()
+    problem_f = tempfile.NamedTemporaryFile(
+        mode="w+t", suffix=".pddl", delete=False)
+    problem_f.close()
+    write_PDDL(state[pddl_task_key], domain_filename=domain_f.name,
+               problem_filename=problem_f.name)
+    return (domain_f.name, problem_f.name)
+
+
+@contextlib.contextmanager
+def state_with_generated_sas_file(state, sas_file_key="generated_sas_filename", sas_task_key="sas_task"):
+    """
+    Context manager that adds an entry for the temporary generated sas file
+    to *state* and removes it afterwards.
+    """
+    # TODO: not sure if modifying the state object directly is a good idea
+    state[sas_file_key] = generate_sas_file(state, sas_task_key)
+    yield state
+    # delete temporary file and delete entry from state
+    os.remove(state[sas_file_key])
+    del state[sas_file_key]
+
+
+@contextlib.contextmanager
+def state_with_generated_pddl_files(state,
+                                    pddl_domain_key="generated_pddl_domain_filename",
+                                    pddl_problem_key="generated_pddl_problem_filename",
+                                    pddl_task_key="pddl_task"):
+    """
+    Context manager that adds entries for the temporary generated domain
+    and problem file to *state* and removes it afterwards.
+    """
+    domain_filename, problem_filename = generate_pddl_files(state, pddl_task_key)
+    # TODO: not sure if modifying the state object directly is a good idea
+    state[pddl_domain_key] = domain_filename
+    state[pddl_problem_key] = problem_filename
+    yield state
+    # delete temporary files and delete entries from state
+    os.remove(state[pddl_domain_key])
+    os.remove(state[pddl_problem_key])
+    del state[pddl_domain_key]
+    del state[pddl_problem_key]
