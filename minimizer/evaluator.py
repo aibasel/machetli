a3e85fcc4e5d9fef090e00105890eaa88776f9aa
Lucas <lucas.galerykaeser@gmail.com>
2021-06-24 11:53:53 +0200

    [evaluators] Implement abstract implementation of Evaluator and write docs.

--- minimizer/evaluator.py
+++ minimizer/evaluator.py
@@ -1,3 +1,30 @@
 class Evaluator:
+    """Interface for state evaluators.
+    """
+    parsers = None
+
     def evaluate(self, state):
+        """Return ``True`` if *state* is accepted and ``False`` otherwise.
+        Must be implemented in derived classes.
+        """
+        raise NotImplementedError()
+
+
+class ParsingEvaluator(Evaluator):
+    """Abstract :class:`Evaluator<minimizer.evaluator.Evaluator>` implementation
+    for output and returncode parsing."""
+
+    def evaluate(self, state):
+        """Call :func:`run_and_parse_all(state, parsers)<minimizer.run.run_and_parse_all>`
+        and return
+        :meth:`self.interpret_results(results)<minimizer.evaluator.ParsingEvaluator.interpret_results>`."""
+        with state_with_generated_pddl_files(state) as local_state:
+            results = run_and_parse_all(local_state, self.parsers)
+        return self.interpret_results(results)
+
+    def interpret_results(self, results):
+        """Return ``True`` or ``False``, depending on the content of
+        the *results* dictionary.
+        Must be implemented in derived classes.
+        """
         raise NotImplementedError()
