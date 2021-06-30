2b213b82e23ad318a0c5b7965fcd8412eafa55f6
Lucas <lucas.galerykaeser@gmail.com>
2021-06-30 20:17:36 +0200

    [improve-setup] Improve setup.

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 setup(
-    name = 'minimizer',
-    packages = find_packages(),
+    name="minimizer",
+    description="Locate bugs in your program",
+    author="Lucas Galery Käser",
+    author_email="lucas.galerykaeser@gmail.com",
+    url="https://github.com/aibasel/minimizer",
+    license="GPL3+",
+    packages=["minimizer", "minimizer.grid", "minimizer.planning"],
+    install_requires=["lab"],
+    python_requires=">=3.7"
 )
