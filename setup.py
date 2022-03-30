baa3dbf1ed0f7cf894b16c005b03a256955ae89d
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2022-03-30 16:42:55 +0200

    [main] Update metadata in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 from setuptools import setup, find_packages
 
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="minimizer",
+    version="0.1",
     description="Locate bugs in your program",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     author="Lucas Galery Käser",
     author_email="lucas.galerykaeser@gmail.com",
     url="https://github.com/aibasel/minimizer",
     license="GPL3+",
+    project_urls={
+        "Documentation": "https://minimizer.readthedocs.io/",
+    },
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+    ],
     packages=find_packages(),
     python_requires=">=3.7",
     include_package_data=True,
