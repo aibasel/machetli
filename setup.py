b7501cb675b6c2e4d23902be77f0369c1be29170
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2022-04-10 16:41:46 +0200

    [main] Update package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name="machetli",
-    version="0.1",
+    version="0.2",
     description="Locate bugs in your program",
     long_description=long_description,
     long_description_content_type="text/x-rst",
