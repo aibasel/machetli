e412583aa0b30d1c85aa3d714bc42033a4b3d4c9
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2022-04-10 16:30:19 +0200

    update name to machetli

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="minimizer",
+    name="machetli",
     version="0.1",
     description="Locate bugs in your program",
     long_description=long_description,
