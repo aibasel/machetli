40a15fd838da768b78a71a61654667579b988441
ClemensBuechner <clemens.buechner@unibas.ch>
2022-04-21 11:44:39 +0200

    Update version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name="machetli",
-    version="0.4",
+    version="0.5",
     description="Locate bugs in your program",
     long_description=long_description,
     long_description_content_type="text/x-rst",
