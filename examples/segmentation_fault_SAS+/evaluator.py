9e4d8c7a3889cf094f392c915ee844b0116abd13
ClemensBuechner <clemens.buechner@unibas.ch>
2022-05-06 18:02:54 +0200

    Unify runs with and without input file.

--- examples/segmentation_fault_SAS+/evaluator.py
+++ examples/segmentation_fault_SAS+/evaluator.py
@@ -12,13 +12,12 @@ def evaluate(state):
     # to a file that is automatically deleted afterwards.
     with sas.temporary_file(state) as sas_filename:
         command = [
-            PLANNER, sas_filename, "--search",
+            PLANNER, "--search",
             "astar(operatorcounting(constraint_generators=[state_equation_constraints()]))"]
         # The downward binary we are using takes stdin arguments, so we use the Run
         # implementation RunWithInputFile which enables us to pass the contents of
         # a file to stdin when *command* is executed.
-        run = tools.RunWithInputFile(
-            command, input_file=f"{sas_filename}", time_limit=10,
-            memory_limit=3338)
+        run = tools.Run(command, input_file=f"{sas_filename}",
+                        time_limit=10, memory_limit=3338)
         stdout, stderr, returncode = run.start()
     return "caught signal 11" in stdout or "caught signal 6" in stdout
--- machetli/tools.py
+++ machetli/tools.py
@@ -216,7 +216,8 @@ class Run:
     """Define an executable command with time and memory limits.
     """
 
-    def __init__(self, command, time_limit=1800, memory_limit=None, log_output=None):
+    def __init__(self, command, time_limit=1800, memory_limit=None,
+                 log_output=None, input_file=None):
         """*command* is a list of strings that starts your program with
         the desired parameters on a Linux machine.
 
@@ -236,9 +237,13 @@ class Run:
         self.memory_limit = memory_limit
         self.log_on_fail = log_output == "on_fail"
         self.log_always = log_output == "always"
+        self.input_file = input_file
 
     def __repr__(self):
-        return f'Run(\"{" ".join([os.path.basename(part) for part in self.command])}\")'
+        cmd = " ".join([os.path.basename(part) for part in self.command])
+        if self.input_file:
+            cmd += f" < {self.input_file}"
+        return f'Run(\"{cmd}\")'
 
     def start(self):
         """Format the command with the entries of *state* and execute it with
@@ -265,129 +270,30 @@ class Run:
 
         logging.debug(f"Command:\n{self.command}")
 
-        try:
-            process = subprocess.Popen(self.command,
-                                       preexec_fn=_prepare_call,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE,
-                                       text=True)
-        except OSError as err:
-            if err.errno == errno.ENOENT:
-                cmd = " ".join(self.command)
-                logging.critical(f"Error: Call '{cmd}' failed. "
-                                 "One of the files was not found.")
-            else:
-                raise
-
-        out_str, err_str = process.communicate()
-
-        return (out_str, err_str, process.returncode)
-
-
-class RunWithInputFile(Run):
-    """Extension of the :class:`Run <machetli.run.Run>` class adding
-    the option of sending the content of a file to stdin.
-    """
-    # e.g., in a command like ``path/to/./my_executable < my_input_file``.
-
-    def __init__(self, command, input_file, **kwargs):
-        """*input_file* is the path to the file whose content should be sent to
-        the stdin of the executed *command*.
-        """
-        super().__init__(command, **kwargs)
-        self.input_file = input_file
-
-    def start(self):
-        """Same as the :meth:`base method <machetli.run.Run.start>`, with
-        the addition of the content from *input_file* being passed to the
-        stdin of the executed *command*.
-        """
-        # These declarations are needed for the _prepare_call() function.
-        time_limit = self.time_limit
-        memory_limit = self.memory_limit
-
-        def _prepare_call():
-            # When the soft time limit is reached, SIGXCPU is emitted. Once we
-            # reach the higher hard time limit, SIGKILL is sent. Having some
-            # padding between the two limits allows programs to handle SIGXCPU.
-            if time_limit is not None:
-                _set_limit(resource.RLIMIT_CPU, time_limit, time_limit + 5)
-            if memory_limit is not None:
-                _, hard_mem_limit = resource.getrlimit(resource.RLIMIT_AS)
-                # Convert memory from MiB to Bytes.
-                _set_limit(resource.RLIMIT_AS, memory_limit *
-                           1024 * 1024, hard_mem_limit)
-            _set_limit(resource.RLIMIT_CORE, 0, 0)
-
-        try:
-            process = subprocess.Popen(self.command,
-                                       preexec_fn=_prepare_call,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE,
-                                       stdin=subprocess.PIPE,
-                                       text=True)
-        except OSError as err:
-            if err.errno == errno.ENOENT:
-                cmd = " ".join(self.command)
-                logging.critical(f"Error: Call '{cmd}' failed. "
-                                 "One of the files was not found.")
-            else:
-                raise
-
-        with open(self.input_file, "r") as file:
-            input_text = file.read()
+        stdin = subprocess.PIPE if self.input_file else None
+        process = subprocess.Popen(self.command,
+                                   preexec_fn=_prepare_call,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   stdin=stdin,
+                                   text=True)
+        input_text = None
+        if self.input_file:
+            with open(self.input_file, "r") as file:
+                input_text = file.read()
 
         out_str, err_str = process.communicate(input=input_text)
 
-        return (out_str, err_str, process.returncode)
+        # TODO: The following block stems from *run_all* and we might want to
+        #  reuse some of its logic.
+        # if run.log_always or run.log_on_fail and returncode != 0:
+        #     cwd = state["cwd"] if "cwd" in state else os.path.dirname(
+        #         get_script_path())
+        #     if stdout:
+        #         with open(os.path.join(cwd, f"{name}.log"), "w") as logfile:
+        #             logfile.write(stdout)
+        #     if stderr:
+        #         with open(os.path.join(cwd, f"{name}.err"), "w") as errfile:
+        #             errfile.write(stderr)
 
-
-def run_all(state):
-    """Start all runs in *state["runs"]* and return a *results* dictionary
-    where run outputs of run *run_name* can be accessed via:
-
-    - *results[run_name]["stdout"]*,
-    - *results[run_name]["stderr"]* and
-    - *results[run_name]["returncode"]*.
-    """
-    assert "runs" in state, "Could not find entry \"runs\" in state."
-    results = {}
-    for name, run in state["runs"].items():
-        stdout, stderr, returncode = run.start(state)
-        if run.log_always or run.log_on_fail and returncode != 0:
-            cwd = state["cwd"] if "cwd" in state else os.path.dirname(
-                get_script_path())
-            if stdout:
-                with open(os.path.join(cwd, f"{name}.log"), "w") as logfile:
-                    logfile.write(stdout)
-            if stderr:
-                with open(os.path.join(cwd, f"{name}.err"), "w") as errfile:
-                    errfile.write(stderr)
-        results.update(
-            {name: {"stdout": stdout, "stderr": stderr, "returncode": returncode}}
-        )
-    return results
-
-
-def run_and_parse_all(state, parsers):
-    """Execute :func:`machetli.run.run_all` and apply all *parsers* to the
-    generated stdout and stderr outputs. Return an updated version of the
-    *results* dictionary containing the parsing results in place of the actual
-    stdout and stderr outputs. *parsers* can be a list of :class:`machetli.parser.Parser`
-    objects or a single one.
-    """
-    results = run_all(state)
-    parsed_results = {}
-    parsers = [parsers] if not isinstance(parsers, list) else parsers
-    for name, result in results.items():
-        parsed_results.update(
-            {name: {"stdout": {}, "stderr": {},
-                    "returncode": result["returncode"]}}
-        )
-        for parser in parsers:
-            parsed_results[name]["stdout"].update(
-                parser.parse(name, result["stdout"]))
-            parsed_results[name]["stderr"].update(
-                parser.parse(name, result["stderr"]))
-    parsed_results["raw_results"] = results
-    return parsed_results
+        return out_str, err_str, process.returncode
