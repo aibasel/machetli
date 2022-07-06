3f0e3715a1fa083fe61dd52179c0ae7c1f830dec
ClemensBuechner <clemens.buechner@unibas.ch>
2022-07-07 00:47:58 +0200

    Update to version 0.6.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name="machetli",
-    version="0.5",
+    version="0.6",
     description="Locate bugs in your program",
     long_description=long_description,
     long_description_content_type="text/x-rst",
