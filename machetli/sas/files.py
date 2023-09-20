58c3c7021936da3816a46eeb739331def64b7fa2
Florian Pommerening <florian.pommerening@unibas.ch>
2023-09-20 15:48:34 +0200

    [issue 59] Clean up SAS+ parser.

--- machetli/sas/files.py
+++ machetli/sas/files.py
@@ -1,6 +1,7 @@
 import tempfile
 import contextlib
 import os
+from pathlib import Path
 
 from machetli.sas.constants import KEY_IN_STATE
 from machetli.sas.sas_tasks import SASTask, SASVariables, SASMutexGroup, \
@@ -17,7 +18,7 @@ def generate_initial_state(sas_file: str) -> dict:
              in the file `sas_file`.
     """
     return {
-        KEY_IN_STATE: _read_task(sas_file)
+        KEY_IN_STATE: _read_task(Path(sas_file))
     }
 
 
@@ -44,112 +45,103 @@ def temporary_file(state: dict) -> str:
     os.remove(f.name)
 
 
-def _read_task(sas_file) -> SASTask:
-    with open(sas_file, "r") as sf:
-        while True:
-            # pos = sf.tell()
-            line = sf.readline().replace("\n", "")
-            if line == "begin_metric":
-                break
-        metric = bool(sf.readline().replace("\n", ""))
-        sf.readline()  # skip end_metric
-        # read variables
-        num_vars = int(sf.readline().replace("\n", ""))
-        variables = _read_variables(sf, num_vars)
-        # read mutexes
-        num_mutexes = int(sf.readline().replace("\n", ""))
-        mutexes = _read_mutexes(sf, num_mutexes)
-        # read init state
-        init = _read_init_state(sf, num_vars)
-        # read goal
-        goal = _read_goal(sf)
-        # read operators
-        num_operators = int(sf.readline().replace("\n", ""))
-        operators = _read_operators(sf, num_operators)
-        # read axioms
-        num_axioms = int(sf.readline().replace("\n", ""))
-        axioms = _read_axioms(sf, num_axioms)
+def _read_task(sas_file : Path) -> SASTask:
+    lines = (l for l in sas_file.read_text().splitlines())
+    while True:
+        line = next(lines)
+        if line == "begin_metric":
+            break
+    metric = bool(next(lines))
+    assert next(lines) == "end_metric"
+    # read variables
+    num_vars = int(next(lines))
+    variables = _read_variables(lines, num_vars)
+    # read mutexes
+    num_mutexes = int(next(lines))
+    mutexes = _read_mutexes(lines, num_mutexes)
+    # read init state
+    init = _read_init_state(lines, num_vars)
+    # read goal
+    goal = _read_goal(lines)
+    # read operators
+    num_operators = int(next(lines))
+    operators = _read_operators(lines, num_operators)
+    # read axioms
+    num_axioms = int(next(lines))
+    axioms = _read_axioms(lines, num_axioms)
 
     sas_task = SASTask(variables, mutexes, init, goal, operators, axioms, metric)
     sas_task.validate()
     return sas_task
 
 
-def _read_variables(sf, num_vars):
+def _read_variables(lines, num_vars):
     axiom_layers = []
     ranges = []
     value_name_lists = []
-    for var in range(num_vars):
-        beginning_line = sf.readline().replace("\n", "")
-        assert beginning_line == "begin_variable"
-        sf.readline()  # skip variable name
-        axiom_layers.append(int(sf.readline().replace("\n", "")))
-        ranges.append(int(sf.readline().replace("\n", "")))
+    for _ in range(num_vars):
+        assert next(lines) == "begin_variable"
+        next(lines)  # skip variable name
+        axiom_layers.append(int(next(lines)))
+        num_values = int(next(lines))
+        ranges.append(num_values)
         value_names = []
-        for name in range(ranges[-1]):
-            value_names.append(sf.readline().replace("\n", ""))
+        for _ in range(num_values):
+            value_names.append(next(lines))
         value_name_lists.append(value_names)
-        ending_line = sf.readline().replace("\n", "")
-        assert ending_line == "end_variable"
+        assert next(lines) == "end_variable"
     return SASVariables(ranges, axiom_layers, value_name_lists)
 
 
-def _read_mutexes(sf, num_mutexes):
+def _read_mutexes(lines, num_mutexes):
     mutexes = []
-    for mutex_group in range(num_mutexes):
-        beginning_line = sf.readline().replace("\n", "")
-        assert beginning_line == "begin_mutex_group"
-        num_facts = int(sf.readline().replace("\n", ""))
+    for _ in range(num_mutexes):
+        assert next(lines) == "begin_mutex_group"
+        num_facts = int(next(lines))
         facts = []
-        for fact in range(num_facts):
-            var, val = map(int, sf.readline().replace("\n", "").split(" "))
+        for _ in range(num_facts):
+            var, val = map(int, next(lines).split(" "))
             facts.append((var, val))
         mutexes.append(SASMutexGroup(facts))
-        ending_line = sf.readline().replace("\n", "")
-        assert ending_line == "end_mutex_group"
+        assert next(lines) == "end_mutex_group"
     return mutexes
 
 
-def _read_init_state(sf, num_vars):
+def _read_init_state(lines, num_vars):
     init = []
-    beginning_line = sf.readline().replace("\n", "")
-    assert beginning_line == "begin_state"
-    for var in range(num_vars):
-        val = int(sf.readline().replace("\n", ""))
+    assert next(lines) == "begin_state"
+    for _ in range(num_vars):
+        val = int(next(lines))
         init.append(val)
-    ending_line = sf.readline().replace("\n", "")
-    assert ending_line == "end_state"
+    assert next(lines) == "end_state"
     return SASInit(init)
 
 
-def _read_goal(sf):
-    beginning_line = sf.readline().replace("\n", "")
-    assert beginning_line == "begin_goal"
-    num_pairs = int(sf.readline().replace("\n", ""))
+def _read_goal(lines):
+    assert next(lines) == "begin_goal"
+    num_pairs = int(next(lines))
     pairs = []
-    for pair in range(num_pairs):
-        var, val = map(int, sf.readline().replace("\n", "").split(" "))
+    for _ in range(num_pairs):
+        var, val = map(int, next(lines).split(" "))
         pairs.append((var, val))
-    ending_line = sf.readline().replace("\n", "")
-    assert ending_line == "end_goal"
+    assert next(lines) == "end_goal"
     return SASGoal(pairs)
 
 
-def _read_operators(sf, num_operators):
+def _read_operators(lines, num_operators):
     operators = []
-    for op in range(num_operators):
-        beginning_line = sf.readline().replace("\n", "")
-        assert beginning_line == "begin_operator"
-        name = "(" + sf.readline().replace("\n", "") + ")"
-        num_prevail_conditions = int(sf.readline().replace("\n", ""))
+    for _ in range(num_operators):
+        assert next(lines) == "begin_operator"
+        name = "(" + next(lines) + ")"
+        num_prevail_conditions = int(next(lines))
         prevail_conditions = []
-        for pre_cond in range(num_prevail_conditions):
-            var, val = map(int, sf.readline().replace("\n", "").split(" "))
+        for _ in range(num_prevail_conditions):
+            var, val = map(int, next(lines).split(" "))
             prevail_conditions.append((var, val))
-        num_effects = int(sf.readline().replace("\n", ""))
+        num_effects = int(next(lines))
         pre_post = []
-        for eff in range(num_effects):
-            effect_line = list(map(int, sf.readline().replace("\n", "").split(" ")))
+        for _ in range(num_effects):
+            effect_line = list(map(int, next(lines).split(" ")))
             num_effect_conditions = effect_line[0]
             cond = []
             for cond_num in range(1, 2 * num_effect_conditions, 2):
@@ -158,31 +150,28 @@ def _read_operators(sf, num_operators):
                 cond.append((var, val))
             var, pre, post = effect_line[-3:]
             pre_post.append((var, pre, post, cond))
-        cost = int(sf.readline().replace("\n", ""))
+        cost = int(next(lines))
         operators.append(SASOperator(name, prevail_conditions, pre_post, cost))
-        ending_line = sf.readline().replace("\n", "")
-        assert ending_line == "end_operator"
+        assert next(lines) == "end_operator"
     return operators
 
 
-def _read_axioms(sf, num_axioms):
+def _read_axioms(lines, num_axioms):
     axioms = []
-    for ax in range(num_axioms):
-        beginning_line = sf.readline().replace("\n", "")
-        assert beginning_line == "begin_rule"
-        length_body = int(sf.readline().replace("\n", ""))
+    for _ in range(num_axioms):
+        assert next(lines) == "begin_rule"
+        length_body = int(next(lines))
         condition = []
-        for fact in range(length_body):
-            var, val = map(int, sf.readline().replace("\n", "").split(" "))
+        for _ in range(length_body):
+            var, val = map(int, next(lines).split(" "))
             condition.append((var, val))
-        effect_line = list(map(int, sf.readline().replace("\n", "").split(" ")))
+        effect_line = list(map(int, next(lines).split(" ")))
         var = effect_line[0]
         val = effect_line[2]
         assert 1 - val == effect_line[1]
         effect = (var, val)
         axioms.append(SASAxiom(condition, effect))
-        ending_line = sf.readline().replace("\n", "")
-        assert ending_line == "end_rule"
+        assert next(lines) == "end_rule"
     return axioms
 
 
