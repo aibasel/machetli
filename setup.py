72766b0509999e9bd8460f96051559cf083ce0eb
Florian Pommerening <florian.pommerening@unibas.ch>
2024-09-05 11:09:33 +0200

    bump version to 0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name="machetli",
-    version="0.8",
+    version="0.9",
     description="Locate bugs in your program",
     long_description=long_description,
     long_description_content_type="text/x-rst",
